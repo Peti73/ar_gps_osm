ffd89afb39aa4ad6cf5da7bbc6441b5f36c4afd5
Peti73 <95430646+Peti73@users.noreply.github.com>
2025-09-15 12:18:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,3 +21,29 @@ window.addEventListener('deviceorientation', e => {
   beta = e.beta;
   gamma = e.gamma;
 });
+
+function latLngToTile(lat, lng, zoom) {
+    const x = Math.floor((lng + 180) / 360 * Math.pow(2, zoom));
+    const y = Math.floor((1 - Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180)) / Math.PI)/2 * Math.pow(2, zoom));
+    return {x, y};
+}
+
+const zoom = 16; // kezdő zoom
+function drawMap() {
+    const tile = latLngToTile(userLat, userLng, zoom);
+    const url = `https://tile.openstreetmap.org/${zoom}/${tile.x}/${tile.y}.png`;
+
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.onload = () => {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.save();
+        ctx.translate(canvas.width/2, canvas.height/2);
+        ctx.rotate(-alpha * Math.PI / 180); // irányítás
+        ctx.drawImage(img, -img.width/2, -img.height/2);
+        ctx.restore();
+    };
+    img.src = url;
+}
+
+setInterval(drawMap, 1000); // frissítés 1s-ként
